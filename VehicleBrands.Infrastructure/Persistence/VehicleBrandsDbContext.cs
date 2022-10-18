d937053d7ded0638a4db6d8edbfd23de9e4445dd
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-18 10:42:51 -0500

    Update: add schema

--- VehicleBrands.Infrastructure/Persistence/VehicleBrandsDbContext.cs
+++ VehicleBrands.Infrastructure/Persistence/VehicleBrandsDbContext.cs
@@ -5,10 +5,15 @@ namespace VehicleBrands.Infrastructure.Persistence
 {
     public class VehicleBrandsDbContext : DbContext
     {
+        private const string FnzSchema = "fnz";
         public VehicleBrandsDbContext(DbContextOptions<VehicleBrandsDbContext> options) : base(options)
         {
         }
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleBrand>()
+                .ToTable("m_marca_vehiculo", FnzSchema);
+        }
         public DbSet<VehicleBrand>? m_marca_vehiculo { get; set; }
     }
 }
