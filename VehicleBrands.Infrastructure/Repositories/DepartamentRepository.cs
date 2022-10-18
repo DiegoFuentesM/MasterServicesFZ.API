0a9d03f6a12ff761a06f05a49e61c89d6d89a7cc
diegoFm10 <diego479@gmail.com>
2022-10-18 18:23:51 -0500

    Add DepartamentRepository on Infrastructure

---
+++ VehicleBrands.Infrastructure/Repositories/DepartamentRepository.cs
@@ -0,0 +1,13 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class DepartamentRepository : SqlRepositoryBase<Departament>, IDepartamentRepository
+    {
+        public DepartamentRepository(SqlDbContext context) : base(context)
+        {
+        }
+    }
+}
