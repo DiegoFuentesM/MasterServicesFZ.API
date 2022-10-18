f211255357cd80af0d4a60da3830742cf35cc656
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-18 15:29:35 -0500

    Add: identification trypes class

---
+++ VehicleBrands.Domain/IdentificactionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterServicesFZ.Domain
+{
+    public class IdentificactionType
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string etiqueta_rpa { get; set; }
+    }
+}
