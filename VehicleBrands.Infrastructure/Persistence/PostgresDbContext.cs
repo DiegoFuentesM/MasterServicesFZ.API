09eda2ccba814e7d63f998565c84af173c8b8c6a
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-18 12:14:45 -0500

    Change: dbcontext name

---
+++ VehicleBrands.Infrastructure/Persistence/PostgresDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Domain;
+
+namespace Infrastructure.Persistence
+{
+    public class PostgresDbContext : DbContext
+    {
+        private const string FnzSchema = "fnz";
+        public PostgresDbContext(DbContextOptions<PostgresDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleBrand>()
+                .ToTable("m_marca_vehiculo", FnzSchema);
+        }
+        public DbSet<VehicleBrand>? m_marca_vehiculo { get; set; }
+    }
+}
