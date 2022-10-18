0c6eef3cf6eb346f4d46045ddf7e484e687661b5
diegoFm10 <diego479@gmail.com>
2022-10-18 18:16:37 -0500

    Add departament controller

---
+++ MasterServicesFZ.API/Controllers/DepartamentController.cs
@@ -0,0 +1,31 @@
+﻿using MasterServicesFZ.Application.Features.Querys.Departaments.GetAllDepartaments;
+using MasterServicesFZ.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DepartamentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DepartamentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Departament), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetDepartaments()
+        {
+            var departaments = await _mediator.Send(new GetAllDepartamentsQuery());
+            return Ok(departaments);
+        }
+    }
+}
