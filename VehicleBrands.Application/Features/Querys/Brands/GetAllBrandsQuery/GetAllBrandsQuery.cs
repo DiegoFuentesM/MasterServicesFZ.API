56f7f94f4f908811e186cb5cd4fc40e5080db49b
diegoFm10 <diego479@gmail.com>
2022-10-18 18:18:38 -0500

    Change namespace

---
+++ VehicleBrands.Application/Features/Querys/Brands/GetAllBrandsQuery/GetAllBrandsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using MasterServicesFZ.Domain;
+
+namespace MasterServicesFZ.Application.Features.Querys.Brands.GetAllBrandsQuery
+{
+    public class GetAllBrandsQuery : IRequest<List<VehicleBrand>>
+    {
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/Brands/GetAllBrandsQuery/GetAllBrandsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Application.Features.Querys.Brands.GetAllBrandsQuery.Vm;
+using MasterServicesFZ.Domain;
+
+namespace MasterServicesFZ.Application.Features.Querys.Brands.GetAllBrandsQuery
+{
+    public class GetAllBrandsQueryHandler : IRequestHandler<GetAllBrandsQuery, List<VehicleBrand>>
+    {
+        private readonly IVehicleBrandRepository _vehicleBrandRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllBrandsQueryHandler(IVehicleBrandRepository vehicleBrandRepository, IMapper mapper)
+        {
+            _vehicleBrandRepository = vehicleBrandRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<VehicleBrand>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+        {
+            var vehicleBrandList = await _vehicleBrandRepository.GetAllAsync();
+            var brands = _mapper.Map<List<VehicleBrand>>(vehicleBrandList.ToList());
+            return brands;
+            //throw new NotImplementedException();
+        }
+    }
+}
