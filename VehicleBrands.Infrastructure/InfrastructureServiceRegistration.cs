c8331e231cf1d73dbe3f3b72c705976e5ca8a998
diegoFm10 <diego479@gmail.com>
2022-10-18 15:13:45 -0500

    Add: sql dbContext service

--- VehicleBrands.Infrastructure/InfrastructureServiceRegistration.cs
+++ VehicleBrands.Infrastructure/InfrastructureServiceRegistration.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Core.Application.Contracts.Persistence;
 using Infrastructure.Persistence;
 using Infrastructure.Repositories;
+using MasterServicesFZ.Infrastructure.Persistence;
 
 namespace Infrastructure
 {
@@ -14,7 +15,10 @@ namespace Infrastructure
             services.AddDbContext<PostgresDbContext>(options =>
                 options.UseNpgsql(configuration.GetConnectionString("PostgreConnection"))
             );
-                
+            services.AddDbContext<SqlDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("SqlConnection"))
+            );
+
             services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
             services.AddScoped<IVehicleBrandRepository, BrandRepository>();
 
