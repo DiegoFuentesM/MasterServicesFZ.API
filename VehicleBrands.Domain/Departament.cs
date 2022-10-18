18823aced55877458692769f0f77d5226d16ef8b
diegoFm10 <diego479@gmail.com>
2022-10-18 16:01:07 -0500

    Add: departament model

---
+++ VehicleBrands.Domain/Departament.cs
@@ -0,0 +1,9 @@
+﻿namespace MasterServicesFZ.Domain
+{
+    public class Departament
+    {
+        public string CodigoLP { get; set; }
+        public string Nombre { get; set; }
+        public int CodigoSrc { get; set; }
+    }
+}
