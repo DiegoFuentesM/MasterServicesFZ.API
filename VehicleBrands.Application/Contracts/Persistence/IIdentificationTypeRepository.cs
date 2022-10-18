b2012696113a68057f10f6c292d3afc47d2aed35
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-18 15:29:12 -0500

    Add: identification types repository

---
+++ VehicleBrands.Application/Contracts/Persistence/IIdentificationTypeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+
+namespace MasterServicesFZ.Application.Contracts.Persistence
+{
+    public interface IIdentificationTypeRepository : IAsyncRepository<IdentificactionType>
+    {
+    }
+}
---
+++ VehicleBrands.Infrastructure/Repositories/IndentificationTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Persistence;
+using Infrastructure.Repositories;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class IndentificationTypeRepository : RepositoryBase<IdentificactionType>, IIdentificationTypeRepository
+    {
+        public IndentificationTypeRepository(PostgresDbContext context) : base(context)
+        {
+        }
+    }
+}
