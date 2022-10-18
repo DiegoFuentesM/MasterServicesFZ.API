ec2d2504fe21349d9494493bf9ee24a9103b3a94
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-18 15:29:58 -0500

    Add: identification types query

---
+++ VehicleBrands.Application/Features/Querys/GetAllIdinticationTypesQuery/GetAllIdentificationTypesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Domain;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.GetAllIdinticationTypesQuery
+{
+    public class GetAllIdentificationTypesQuery : IRequest<List<IdentificactionType>>
+    {
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/GetAllIdinticationTypesQuery/GetAllIdentificationTypesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.GetAllIdinticationTypesQuery
+{
+    public class GetAllIdentificationTypesQueryHandler : IRequestHandler<GetAllIdentificationTypesQuery, List<IdentificactionType>
+    {
+        private readonly IIdentificationTypeRepository _identificationRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllIdentificationTypesQueryHandler(IIdentificationTypeRepository identificationRepository, IMapper mapper)
+        {
+            _identificationRepository = identificationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<IdentificactionType>> Handle(GetAllIdentificationTypesQuery request, CancellationToken cancellationToken)
+        {
+            var listIdentificationTypes = await _identificationRepository.GetAllAsync();
+            var identificationTypes = _mapper.Map<List<IdentificactionType>>(listIdentificationTypes);
+            return identificationTypes;
+        }
+           
+    }
+}
