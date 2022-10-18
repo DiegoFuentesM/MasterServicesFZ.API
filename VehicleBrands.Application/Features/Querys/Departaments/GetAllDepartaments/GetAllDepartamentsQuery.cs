4086272c1b957292ea83bdecbd287abb07d1ec41
diegoFm10 <diego479@gmail.com>
2022-10-18 18:19:29 -0500

    Add getAllDepartaments Query and queryHandler

---
+++ VehicleBrands.Application/Features/Querys/Departaments/GetAllDepartaments/GetAllDepartamentsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Domain;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.Departaments.GetAllDepartaments
+{
+    public class GetAllDepartamentsQuery : IRequest<List<Departament>>
+    {
+    }
+}
