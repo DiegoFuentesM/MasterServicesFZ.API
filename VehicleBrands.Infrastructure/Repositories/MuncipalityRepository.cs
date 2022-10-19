5441bdb069e387be7d442ba97656ce97923d9b92
diegoFm10 <diego479@gmail.com>
2022-10-19 14:32:18 -0500

    Add: repository

---
+++ VehicleBrands.Infrastructure/Repositories/MuncipalityRepository.cs
@@ -0,0 +1,13 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class MuncipalityRepository : SqlRepositoryBase<Municipality>, IMunicipalityRepository
+    {
+        public MuncipalityRepository(SqlDbContext context) : base(context)
+        {
+        }
+    }
+}
