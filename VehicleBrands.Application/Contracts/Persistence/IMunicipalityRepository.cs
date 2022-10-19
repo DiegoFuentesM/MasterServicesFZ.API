f8b013d25cb3b370cdbae5ec7c80beb81f84d8fa
diegoFm10 <diego479@gmail.com>
2022-10-19 14:31:58 -0500

    Add: interface repository

---
+++ VehicleBrands.Application/Contracts/Persistence/IMunicipalityRepository.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Domain;
+
+namespace MasterServicesFZ.Application.Contracts.Persistence
+{
+    public interface IMunicipalityRepository : IAsyncRepository<Municipality>
+    {
+        Task<IEnumerable<Municipality>> GetAllMunicipalityByDepartamentQuery(string departamentId);
+    }
+}
