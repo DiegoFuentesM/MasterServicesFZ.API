8436178302717dc459c28c4ad39ede97f83f4c25
diegoFm10 <diego479@gmail.com>
2022-10-19 14:33:42 -0500

    Add: getAllMunicipalityByDepartament query and queryHandler

---
+++ VehicleBrands.Application/Features/Querys/Municipalities/GetAllMunicipalityByDepartament/GetAllMunicipalityByDepartamentQuery.cs
@@ -0,0 +1,15 @@
+﻿using MasterServicesFZ.Domain;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.Municipalities.GetMunicipalityByDepartament
+{
+    public class GetAllMunicipalityByDepartamentQuery : IRequest<List<Municipality>>
+    {
+        public string _departament { get; set; } = String.Empty;
+
+        public GetAllMunicipalityByDepartamentQuery(string departamentId)
+        {
+            _departament = departamentId ?? throw new ArgumentNullException(nameof(departamentId));
+        }
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/Municipalities/GetAllMunicipalityByDepartament/GetMunicipalityByDepartamentQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.Municipalities.GetMunicipalityByDepartament
+{
+    public class GetMunicipalityByDepartamentQueryHandler : IRequestHandler<GetAllMunicipalityByDepartamentQuery, List<Municipality>>
+    {
+        private readonly IMunicipalityRepository _municipalityRepository;
+        private readonly IMapper _mapper;
+
+        public GetMunicipalityByDepartamentQueryHandler(IMunicipalityRepository municipalityRepository, IMapper mapper)
+        {
+            _municipalityRepository = municipalityRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<Municipality>> Handle(GetAllMunicipalityByDepartamentQuery request, CancellationToken cancellationToken)
+        {
+            var municipalityEntity = _mapper.Map<Municipality>(request);
+            var municipalityList = await _municipalityRepository.GetAllMunicipalityByDepartamentQuery(request._departament).ToList();
+            return municipalityList;
+        }
+    }
+}
