796d306ccc1e98e8859c1905f23a5e266766b7c0
diegoFm10 <diego479@gmail.com>
2022-10-19 14:32:43 -0500

    Add: municipality model

---
+++ VehicleBrands.Domain/Municipality.cs
@@ -0,0 +1,9 @@
+﻿namespace MasterServicesFZ.Domain
+{
+    public class Municipality
+    {
+        public string? CodigoLP { get; set; }
+        public string Nombre { get; set; }
+        public string Departamento_Id { get; set; }
+    }
+}
