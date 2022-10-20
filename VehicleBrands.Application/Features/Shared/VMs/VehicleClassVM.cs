4faa60f3fde66740b1e9cfc093d37ca27d159547
diegoFm10 <diego479@gmail.com>
2022-10-20 12:04:19 -0500

    Add vehicle class brand view model

---
+++ VehicleBrands.Application/Features/Shared/VMs/VehicleClassVM.cs
@@ -0,0 +1,8 @@
+﻿namespace MasterServicesFZ.Application.Features.Shared.VMs
+{
+    public class VehicleClassVM
+    {
+        public string clase_vehiculo_id { get; set; }
+        public string clasevehiculo { get; set; }
+    }
+}
