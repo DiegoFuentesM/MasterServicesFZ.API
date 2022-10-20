3783b3edc45223a496166ac6bd1452b7d16ce818
diegoFm10 <diego479@gmail.com>
2022-10-20 12:41:36 -0500

    Add fee controller

---
+++ MasterServicesFZ.API/Controllers/FeeController.cs
@@ -0,0 +1,31 @@
+﻿using MasterServicesFZ.Application.Features.Querys.Fees.GetAllFees;
+using MasterServicesFZ.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class FeeController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public FeeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Fee), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetAllFees()
+        {
+            var fees = await _mediator.Send(new GetAllFeesQuery());
+            return Ok(fees);
+        }
+    }
+}
