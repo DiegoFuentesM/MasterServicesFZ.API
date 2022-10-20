f83e949b6cf968c07b4fef4d4f2d3e8d61f13590
diegoFm10 <diego479@gmail.com>
2022-10-20 12:42:45 -0500

    Add fee dbSet and model builder

--- VehicleBrands.Infrastructure/Persistence/PostgresDbContext.cs
+++ VehicleBrands.Infrastructure/Persistence/PostgresDbContext.cs
@@ -22,10 +22,14 @@ namespace MasterServicesFZ.Infrastructure.Persistence
 
             modelBuilder.Entity<VehicleClassBrand>()
                 .ToTable("m_marca_vehiculo_clase_vehiculo", FnzSchema);
+
+            modelBuilder.Entity<Fee>()
+                .ToTable("m_tasas", FnzSchema);
         }
         public DbSet<VehicleBrand>? m_marca_vehiculo { get; set; }
         public DbSet<IdentificactionType> m_tipo_identificacion { get; set; }
         public DbSet<VehicleType> m_tipo_vehiculo { get; set; }
         public DbSet<VehicleClassBrand> m_marca_vehiculo_clase_vehiculo { get; set; }
+        public DbSet<Fee> m_tasas { get; set; }
     }
 }
