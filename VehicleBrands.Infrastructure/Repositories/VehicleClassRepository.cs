6f8872b93be40bfbea4b02d1d2ec8baa3a1774f9
diegoFm10 <diego479@gmail.com>
2022-10-20 12:05:49 -0500

    Add vehicle class repository interface implementation

---
+++ VehicleBrands.Infrastructure/Repositories/VehicleClassRepository.cs
@@ -0,0 +1,19 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class VehicleClassRepository : RepositoryBase<VehicleClassBrand>, IVehicleClassRepository
+    {
+        public VehicleClassRepository(PostgresDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<VehicleClassBrand>> GetClassByBrandId(string brandId)
+        {
+            return await _context.m_marca_vehiculo_clase_vehiculo.Where(c => c.marca_vehiculo_id == brandId).ToListAsync();
+        }
+    }
+}
