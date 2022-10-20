a9100ab04312e0d4896cf2a44af3f17a0fc4006d
diegoFm10 <diego479@gmail.com>
2022-10-20 12:03:38 -0500

    Add query and handler to get all vehicle classes by brand

---
+++ VehicleBrands.Application/Features/Querys/VehicleClass/GetAllVehicleClassesByBrand/GetAllVehicleClassesByBrandQuery.cs
@@ -0,0 +1,15 @@
+﻿using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.VehicleClass.GetAllVehicleClassesByBrand
+{
+    public class GetAllVehicleClassesByBrandQuery : IRequest<List<VehicleClassVM>>
+    {
+        public string _BrandId { get; set; } = String.Empty;
+
+        public GetAllVehicleClassesByBrandQuery(string brandId)
+        {
+            _BrandId = brandId ?? throw new ArgumentNullException(nameof(brandId));
+        }
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/VehicleClass/GetAllVehicleClassesByBrand/GetAllVehicleClassesByBrandQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.VehicleClass.GetAllVehicleClassesByBrand
+{
+    public class GetAllVehicleClassesByBrandQueryHandler : IRequestHandler<GetAllVehicleClassesByBrandQuery, List<VehicleClassVM>>
+    {
+        private readonly IVehicleClassRepository _vehicleClassRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllVehicleClassesByBrandQueryHandler(IVehicleClassRepository vehicleClassRepository, IMapper mapper)
+        {
+            _vehicleClassRepository = vehicleClassRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<VehicleClassVM>> Handle(GetAllVehicleClassesByBrandQuery request, CancellationToken cancellationToken)
+        {
+            var classList = await _vehicleClassRepository.GetClassByBrandId(request._BrandId);
+            return _mapper.Map<List<VehicleClassVM>>(classList);
+        }
+    }
+}
