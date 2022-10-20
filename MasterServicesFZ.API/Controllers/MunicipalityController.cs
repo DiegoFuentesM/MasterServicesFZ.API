9d7bf1282da27c4bec26438c51fd27fa1fd56e85
diegoFm10 <diego479@gmail.com>
2022-10-20 10:29:06 -0500

    Add: municipality controller

---
+++ MasterServicesFZ.API/Controllers/MunicipalityController.cs
@@ -0,0 +1,31 @@
+﻿using MasterServicesFZ.Application.Features.Querys.Municipalities.GetMunicipalityByDepartament;
+using MasterServicesFZ.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class MunicipalityController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MunicipalityController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Municipality), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetMunicipalities(string departamentId)
+        {
+            var municipalities = await _mediator.Send(new GetAllMunicipalityByDepartamentQuery(departamentId));
+            return Ok(municipalities);
+        }
+    }
+}
