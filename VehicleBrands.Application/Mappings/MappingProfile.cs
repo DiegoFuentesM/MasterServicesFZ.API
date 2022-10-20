d654b3dee671872f0eccc20e5f0ebff813da45c6
diegoFm10 <diego479@gmail.com>
2022-10-20 12:04:40 -0500

    Add view models mapping

---
+++ VehicleBrands.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MasterServicesFZ.Domain;
+
+namespace MasterServicesFZ.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<VehicleClassBrand, VehicleClassVM>();
+        }
+
+    }
+}
