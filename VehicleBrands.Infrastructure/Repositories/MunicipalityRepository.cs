320c2d376afc46dff644341154ce73a8a8797bb5
diegoFm10 <diego479@gmail.com>
2022-10-20 10:37:01 -0500

    change file name

---
+++ VehicleBrands.Infrastructure/Repositories/MunicipalityRepository.cs
@@ -0,0 +1,21 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class MunicipalityRepository : SqlRepositoryBase<Municipality>, IMunicipalityRepository
+    {
+        public MunicipalityRepository(SqlDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Municipality>> GetAllMunicipalityByDepartamentQuery(string departamentId)
+        {
+            var municipalities = await _context.Scr_Municipio!.Where(m => m.Departamento_Id == departamentId).ToListAsync();
+            return municipalities;
+        }
+
+    }
+}
