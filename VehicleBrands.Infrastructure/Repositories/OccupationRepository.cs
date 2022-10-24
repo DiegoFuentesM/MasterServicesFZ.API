f369ad78c351a27b58109c83029968fc2b99a28a
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-24 15:16:13 -0500

    Add occupation repository

---
+++ VehicleBrands.Infrastructure/Repositories/OccupationRepository.cs
@@ -0,0 +1,13 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class OccupationRepository : SqlRepositoryBase<Occupation>, IOccupationRepository
+    {
+        public OccupationRepository(SqlDbContext context) : base(context)
+        {
+        }
+    }
+}
