b1989aedb8d0eb3c77e0c4a3bade76d4ddde89df
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-24 15:18:43 -0500

    Add occupation query

---
+++ VehicleBrands.Application/Features/Querys/Occupations/GetAllOccupationsQuery/GetAllOccupationsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Domain;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.Occupations.GetAllOccupationsQuery
+{
+    public class GetAllOccupationsQuery : IRequest<List<Occupation>>
+    {
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/Occupations/GetAllOccupationsQuery/GetAllOccupationsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.Occupations.GetAllOccupationsQuery
+{
+    public class GetAllOccupationsQueryHandler : IRequestHandler<GetAllOccupationsQuery, List<Occupation>>
+    {
+        private readonly IOccupationRepository _occupationRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllOccupationsQueryHandler(IOccupationRepository occupationRepository, IMapper mapper)
+        {
+            _occupationRepository = occupationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<Occupation>> Handle(GetAllOccupationsQuery request, CancellationToken cancellationToken)
+        {
+            var listOccupations = await _occupationRepository.GetAllAsync();
+            var occupations = _mapper.Map<List<Occupation>>(listOccupations);
+            return occupations;
+        }
+    }
+}
