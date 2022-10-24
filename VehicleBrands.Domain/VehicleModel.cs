913c145bd3895e648592779f6d3192b2c194dbd2
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-24 15:37:26 -0500

    Add vehicleModel model

---
+++ VehicleBrands.Domain/VehicleModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MasterServicesFZ.Domain
+{
+    public class VehicleModel
+    {
+        public int id { get; set; }
+        public bool activo{ get; set; }
+        public int precio { get; set; }
+        public int modelovehiculo_id { get; set; }
+        public int tipovehiculo_id { get; set; }
+        public int modelobienvehiculo_id { get; set; }
+        public bool estado { get; set; }
+    }
+}
