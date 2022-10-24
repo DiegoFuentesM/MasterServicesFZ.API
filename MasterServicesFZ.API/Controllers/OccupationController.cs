34049aa358307548545e8b5b64e8cb8cba899543
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-24 15:14:31 -0500

    Add occupation controller

---
+++ MasterServicesFZ.API/Controllers/OccupationController.cs
@@ -0,0 +1,25 @@
+﻿using MasterServicesFZ.Application.Features.Querys.Occupations.GetAllOccupationsQuery;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OccupationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OccupationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOccupations()
+        {
+            var occupations = await _mediator.Send(new GetAllOccupationsQuery());
+            return Ok(occupations);
+        }
+    }
+}
