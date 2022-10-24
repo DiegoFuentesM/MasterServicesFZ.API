d19ed62d0b3d7e3c347581757ac237b4f5e0a981
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-24 15:35:32 -0500

    Add vehicleModel repository

---
+++ VehicleBrands.Infrastructure/Repositories/VehicleModelRepository.cs
@@ -0,0 +1,28 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class VehicleModelRepository : RepositoryBase<VehicleModel>, IVehicleModelRepository
+    {
+        public VehicleModelRepository(PostgresDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<int>> GetAllModelsByVehicleType(int vehicleTypeID)
+        {
+            //var prueba = await _context.m_precio_fasecolda_nuevo.ToListAsync();
+
+            var models = await _context.m_precio_fasecolda_nuevo
+                .Where(x => x.precio >= 0 && x.estado && x.tipovehiculo_id == vehicleTypeID && x.modelobienvehiculo_id != 0)
+                .Select(x => x.modelovehiculo_id)
+                .Concat(_context.m_precio_fasecolda_nuevo
+                .Where(x => x.precio >= 0 && x.estado && x.tipovehiculo_id == vehicleTypeID && x.modelobienvehiculo_id != 0)
+                .Select(x => x.modelobienvehiculo_id)).Distinct().ToListAsync();
+
+            return models;
+        }
+    }
+}
