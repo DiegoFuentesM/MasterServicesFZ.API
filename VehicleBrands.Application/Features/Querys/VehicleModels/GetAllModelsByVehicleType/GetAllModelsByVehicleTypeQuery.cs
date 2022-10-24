a81ccb34a2a472e70ffffe359937b41e35b27d48
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-24 15:36:54 -0500

    Add get all vehicle model by vehicle type query

---
+++ VehicleBrands.Application/Features/Querys/VehicleModels/GetAllModelsByVehicleType/GetAllModelsByVehicleTypeQuery.cs
@@ -0,0 +1,15 @@
+﻿using MasterServicesFZ.Domain;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.VehicleModels.GetAllModelsByVehicleType
+{
+    public class GetAllModelsByVehicleTypeQuery : IRequest<List<int>>
+    {
+        public int VehicleTypeId { get; set; }
+
+        public GetAllModelsByVehicleTypeQuery(int vehicleTypeId)
+        {
+            VehicleTypeId = vehicleTypeId;
+        }
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/VehicleModels/GetAllModelsByVehicleType/GetAllModelsByVehicleTypeQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.VehicleModels.GetAllModelsByVehicleType
+{
+    public class GetAllModelsByVehicleTypeQueryHandler : IRequestHandler<GetAllModelsByVehicleTypeQuery, List<int>>
+    {
+        private readonly IVehicleModelRepository _vehicleModelRepository;
+
+        public GetAllModelsByVehicleTypeQueryHandler(IVehicleModelRepository vehicleModelRepository, IMapper mapper)
+        {
+            _vehicleModelRepository = vehicleModelRepository;
+        }
+
+        public async Task<List<int>> Handle(GetAllModelsByVehicleTypeQuery request, CancellationToken cancellationToken)
+        {
+            return await _vehicleModelRepository.GetAllModelsByVehicleType(request.VehicleTypeId);
+        }
+    }
+}
