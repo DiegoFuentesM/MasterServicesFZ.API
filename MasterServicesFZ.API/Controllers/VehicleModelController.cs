f34d638f01080c203b356aef70f705417cea7d07
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-24 15:35:55 -0500

    Add vehicle model controller

---
+++ MasterServicesFZ.API/Controllers/VehicleModelController.cs
@@ -0,0 +1,27 @@
+﻿using MasterServicesFZ.Application.Features.Querys.VehicleModels.GetAllModelsByVehicleType;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class VehicleModelController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public VehicleModelController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [HttpGet("{vehicleTypeId}")]
+        public async Task<IActionResult> GetAllVehicleModels(int vehicleTypeId)
+        {
+            var models = await _mediator.Send(new GetAllModelsByVehicleTypeQuery(vehicleTypeId));
+            return Ok(models);
+        }
+    }
+}
