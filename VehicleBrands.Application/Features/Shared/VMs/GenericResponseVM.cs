c569603e442d46aeea38499044bc7791e2f4c4a4
diegoFm10 <diego479@gmail.com>
2022-11-03 09:47:08 -0500

    Refactor unused view model to GenericResponse view model

---
+++ VehicleBrands.Application/Features/Shared/VMs/GenericResponseVM.cs
@@ -0,0 +1,8 @@
+﻿namespace MasterServicesFZ.Application.Features.Shared.VMs
+{
+    public class GenericResponseVM
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+}
--- VehicleBrands.Application/Features/Shared/VMs/VehicleClassVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MasterServicesFZ.Application.Features.Shared.VMs
-{
-    public class VehicleClassVM
-    {
-        public string clase_vehiculo_id { get; set; }
-        public string clasevehiculo { get; set; }
-    }
-}
