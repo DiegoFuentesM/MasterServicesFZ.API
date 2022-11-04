3eb5ac7bee48121ada871fa7f05c042b38841f9b
YanMorera <90201379+YanMorera@users.noreply.github.com>
2022-11-04 13:31:53 -0500

    Add: SwaggerConfiguration

---
+++ MasterServicesFZ.API/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace MasterServicesFZ.API.Configuration
+{
+    public static class SwaggerConfiguration
+    {
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Servicios Maestros Api V1",
+                    Description = "Servicios Maestros Api V1",
+                });
+                options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                List<string> xmlFiles = Directory.GetFiles(AppContext.BaseDirectory, "*.xml", SearchOption.TopDirectoryOnly).ToList();
+                xmlFiles.ForEach(xmlFile => options.IncludeXmlComments(xmlFile));
+
+                options.AddSecurityDefinition("XApiKey", new OpenApiSecurityScheme
+                {
+                    Description = "ApiKey must appear in header",
+                    Type = SecuritySchemeType.ApiKey,
+                    Name = "XApiKey",
+                    In = ParameterLocation.Header,
+                    Scheme = "ApiKeyScheme"
+                });
+                var key = new OpenApiSecurityScheme()
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "XApiKey"
+                    },
+                    In = ParameterLocation.Header
+                };
+                var requirement = new OpenApiSecurityRequirement
+                    {
+                             { key, new List<string>() }
+                    };
+                options.AddSecurityRequirement(requirement);
+            });
+            return services;
+        }
+    }
+}
