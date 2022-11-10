5cfca72911fb954ea933a6b9d4c444c8e9125983
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 12:14:32 -0600

    Add controller contract types

---
+++ MasterServicesFZ.API/Controllers/ContractTypeController.cs
@@ -0,0 +1,31 @@
+﻿using MasterServicesFZ.Application.Features.Querys.ContractTypes.GetAllContractTypes;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ContractTypeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ContractTypeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(GenericResponseVM), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetContractTypes()
+        {
+            var contractTypes = await _mediator.Send(new GetAllContractTypesQuery());
+            return Ok(contractTypes);
+        }
+    }
+}
