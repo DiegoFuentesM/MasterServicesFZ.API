2eee58c3f6b3efe03b63c56213e2001272e78842
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 14:06:53 -0600

    Add account type  controller

---
+++ MasterServicesFZ.API/Controllers/AccountTypeController.cs
@@ -0,0 +1,30 @@
+﻿using MasterServicesFZ.Application.Features.Querys.AccountTypes.GetAllActiveAccountTypes;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AccountTypeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AccountTypeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(GenericResponseVM), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetAllActive()
+        {
+            var accountTypes = await _mediator.Send(new GetAllActiveAccountTypesQuery());
+            return Ok(accountTypes);
+        }
+    }
+}
