74ecc6676ff08659877db289d199507e0cc42b51
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 14:07:05 -0600

    Add account type repository

---
+++ VehicleBrands.Infrastructure/Repositories/AccountTypeRepository.cs
@@ -0,0 +1,19 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class AccountTypeRepository : CreditProductsRepositoryBase<AccountType>, IAccountTypeRepository
+    {
+        public AccountTypeRepository(CreditProductsDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<AccountType>> GetAllActive()
+        {
+            return await _context.TiposCuenta.Where(x => x.Activo).ToListAsync();
+        }
+    }
+}
