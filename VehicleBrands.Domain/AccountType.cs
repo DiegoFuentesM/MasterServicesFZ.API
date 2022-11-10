1c71964e0154bbda2d32672edc51fc3270594a12
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 14:05:13 -0600

    Add account type model

---
+++ VehicleBrands.Domain/AccountType.cs
@@ -0,0 +1,9 @@
+﻿namespace MasterServicesFZ.Domain
+{
+    public class AccountType
+    {
+        public int TipoCuentaID { get; set; }
+        public string Nombre { get; set; }
+        public bool Activo { get; set; }
+    }
+}
