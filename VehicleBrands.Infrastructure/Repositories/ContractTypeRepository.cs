4497683d7b7d9f3ec428e2fd0ce11a241f660f75
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 12:15:14 -0600

    Add repository contract types

---
+++ VehicleBrands.Infrastructure/Repositories/ContractTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    internal class ContractTypeRepository : SqlRepositoryBase<ContractType>, IContractTypeRepository
+    {
+        public ContractTypeRepository(SqlDbContext context) : base(context)
+        {
+        }
+    }
+}
