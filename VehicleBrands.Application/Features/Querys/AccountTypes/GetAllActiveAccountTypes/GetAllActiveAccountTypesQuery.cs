095577adec26e5455bf2ef62235cae16993964f2
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 14:04:44 -0600

    Add get all active account types query

---
+++ VehicleBrands.Application/Features/Querys/AccountTypes/GetAllActiveAccountTypes/GetAllActiveAccountTypesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.AccountTypes.GetAllActiveAccountTypes
+{
+    public class GetAllActiveAccountTypesQuery : IRequest<List<GenericResponseVM>>
+    {
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/AccountTypes/GetAllActiveAccountTypes/GetAllActiveAccountTypesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.AccountTypes.GetAllActiveAccountTypes
+{
+    public class GetAllActiveAccountTypesQueryHandler : IRequestHandler<GetAllActiveAccountTypesQuery, List<GenericResponseVM>>
+    {
+        private readonly IAccountTypeRepository _accountTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllActiveAccountTypesQueryHandler(IAccountTypeRepository accountTypeRepository, IMapper mapper)
+        {
+            _accountTypeRepository = accountTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GenericResponseVM>> Handle(GetAllActiveAccountTypesQuery request, CancellationToken cancellationToken)
+        {
+            var accountTypesList = await _accountTypeRepository.GetAllActive();
+            var accountTypes = _mapper.Map<List<GenericResponseVM>>(accountTypesList);
+            return accountTypes;
+        }
+    }
+}
