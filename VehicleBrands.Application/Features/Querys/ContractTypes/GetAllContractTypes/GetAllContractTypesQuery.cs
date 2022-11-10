de2dc03785cb2075f4a354b5ade6c258b2b93f68
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 12:13:39 -0600

    Add get all contract types query

---
+++ VehicleBrands.Application/Features/Querys/ContractTypes/GetAllContractTypes/GetAllContractTypesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.ContractTypes.GetAllContractTypes
+{
+    public class GetAllContractTypesQuery : IRequest<List<GenericResponseVM>>
+    {
+    }
+}
