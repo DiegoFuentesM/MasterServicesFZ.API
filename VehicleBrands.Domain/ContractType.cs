211732521b10afa7c1387f33fd3afce1f36d5c9f
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 12:11:11 -0600

    Add contract types model

---
+++ VehicleBrands.Domain/ContractType.cs
@@ -0,0 +1,9 @@
+﻿namespace MasterServicesFZ.Domain
+{
+    public class ContractType
+    {
+        public int CodigoLP { get; set; }
+        public string Descripcion { get; set; }
+        public int CodigoScr { get; set; }
+    }
+}
