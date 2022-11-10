e704901f6bf97809dd5cda458917a25eead2d537
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 12:14:02 -0600

    Add get all contract types query handler

---
+++ VehicleBrands.Application/Features/Querys/ContractTypes/GetAllContractTypes/GetAllContractTypesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.ContractTypes.GetAllContractTypes
+{
+    internal class GetAllContractTypesQueryHandler : IRequestHandler<GetAllContractTypesQuery, List<GenericResponseVM>>
+    {
+        private readonly IContractTypeRepository _contractTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllContractTypesQueryHandler(IContractTypeRepository contractTypeRepository, IMapper mapper)
+        {
+            _contractTypeRepository = contractTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GenericResponseVM>> Handle(GetAllContractTypesQuery request, CancellationToken cancellationToken)
+        {
+            var contractTypesList = await _contractTypeRepository.GetAllAsync();
+            var contractTypes = _mapper.Map<List<GenericResponseVM>>(contractTypesList);
+            return contractTypes;
+        }
+    }
+}
