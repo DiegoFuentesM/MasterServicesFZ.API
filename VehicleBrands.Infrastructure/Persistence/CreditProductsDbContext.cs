5924583f5c39846c30fd45e2520ddf1939a0620c
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 14:04:10 -0600

    Add credit products context

---
+++ VehicleBrands.Infrastructure/Persistence/CreditProductsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using MasterServicesFZ.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasterServicesFZ.Infrastructure.Persistence
+{
+    public class CreditProductsDbContext : DbContext
+    {
+        private const string DboSchema = "CRE";
+
+        public CreditProductsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AccountType>()
+                .ToTable("TiposCuenta", DboSchema)
+                .HasKey(m => m.TipoCuentaID);
+        }
+        public DbSet<AccountType> TiposCuenta { get; set; }
+    }
+}
