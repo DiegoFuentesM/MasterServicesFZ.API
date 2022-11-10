d2deebe5ea0474b13d28be74825f85fb9c39709d
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-10 12:10:56 -0600

    Add(context): contract types

--- VehicleBrands.Infrastructure/Persistence/SqlDbContext.cs
+++ VehicleBrands.Infrastructure/Persistence/SqlDbContext.cs
@@ -24,10 +24,15 @@ namespace MasterServicesFZ.Infrastructure.Persistence
             modelBuilder.Entity<Occupation>()
                 .ToTable("Scr_Ocupacion", DboSchema)
                 .HasKey(m => m.CodigoLP);
+
+            modelBuilder.Entity<ContractType>()
+                .ToTable("Scr_TipoContrato", DboSchema)
+                .HasKey(m => m.CodigoLP);
         }
 
         public DbSet<Departament>? Scr_Departamento { get; set; }
         public DbSet<Municipality>? Scr_Municipio { get; set; }
         public DbSet<Occupation>? Scr_Ocupacion { get; set; }
+        public DbSet<ContractType>? Scr_TipoContrato { get; set; }
     }
 }
