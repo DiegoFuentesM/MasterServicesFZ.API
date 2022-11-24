27064f77da6e6ac7809c084169baa799a3cb5fe8
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 14:16:56 -0600

    Add query handler to get all marital statuses

---
+++ VehicleBrands.Application/Features/Querys/MaritalStatuses/GetAllMaritalStatusesQuery/GetAllMaritalStatusesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.MaritalStatuses.GetAllMaritalStatusesQuery
+{
+    public class GetAllMaritalStatusesQueryHandler : IRequestHandler<GetAllMaritalStatusesQuery, List<GenericResponseVM>>
+    {
+        private readonly IMaritalStatusRepository _maritalStatusRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllMaritalStatusesQueryHandler(IMaritalStatusRepository maritalStatusRepository, IMapper mapper)
+        {
+            _maritalStatusRepository = maritalStatusRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GenericResponseVM>> Handle(GetAllMaritalStatusesQuery request, CancellationToken cancellationToken)
+        {
+            var listMaritalStatuses = await _maritalStatusRepository.GetAllAsync();
+            var maritalStatuses = _mapper.Map<List<GenericResponseVM>>(listMaritalStatuses).OrderBy(x => x.Value).ToList();
+            return maritalStatuses;
+        }
+    }
+}
