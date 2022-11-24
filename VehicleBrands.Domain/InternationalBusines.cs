a9a8b19e6a542d88738bdcc31cdf97ca4cd086af
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 15:52:10 -0600

    Add international business model

---
+++ VehicleBrands.Domain/InternationalBusines.cs
@@ -0,0 +1,8 @@
+﻿namespace MasterServicesFZ.Domain
+{
+    public class InternationalBusiness 
+    {
+        public int id { get; set; }
+        public string tipo_operacion { get; set; }
+    }
+}
