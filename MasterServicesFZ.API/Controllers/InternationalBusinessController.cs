e4068761ec39118e2f47c0ad306b7f4c8c7e6116
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 16:45:08 -0600

    Add international business controller

---
+++ MasterServicesFZ.API/Controllers/InternationalBusinessController.cs
@@ -0,0 +1,30 @@
+﻿using MasterServicesFZ.Application.Features.Querys.InternationalBusiness.GetAllInternationalBusiness;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class InternationalBusinessController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public InternationalBusinessController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(GenericResponseVM), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetAll()
+        {
+            var internationalBusiness = await _mediator.Send(new GetAllInternationalBusinessQuery());
+            return Ok(internationalBusiness);
+        }
+    }
+}
