c403268f2f729f2c8507bcfe21151cfaf1f92b32
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 14:15:25 -0600

    Add repository marital status

---
+++ VehicleBrands.Infrastructure/Repositories/MaritalStatusRepository.cs
@@ -0,0 +1,14 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public
+        class MaritalStatusRepository : SqlRepositoryBase<MaritalStatus>, IMaritalStatusRepository
+    {
+        public MaritalStatusRepository(SqlDbContext context) : base(context)
+        {
+        }
+    }
+}
