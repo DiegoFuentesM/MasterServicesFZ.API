fd67ee872705a2c0968c522ed963bb8364b5f859
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 14:16:39 -0600

    Add query to get all marital statuses

---
+++ VehicleBrands.Application/Features/Querys/MaritalStatuses/GetAllMaritalStatusesQuery/GetAllMaritalStatusesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.MaritalStatuses.GetAllMaritalStatusesQuery
+{
+    public class GetAllMaritalStatusesQuery : IRequest<List<GenericResponseVM>>
+    {
+    }
+}
