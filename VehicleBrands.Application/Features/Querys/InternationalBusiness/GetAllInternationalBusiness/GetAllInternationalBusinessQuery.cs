41ca5ca795c77835d2a01321cd7fa37b1db09ace
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 16:45:40 -0600

    Add query to get all International business

---
+++ VehicleBrands.Application/Features/Querys/InternationalBusiness/GetAllInternationalBusiness/GetAllInternationalBusinessQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.InternationalBusiness.GetAllInternationalBusiness
+{
+    public class GetAllInternationalBusinessQuery : IRequest<List<GenericResponseVM>>
+    {
+    }
+}
