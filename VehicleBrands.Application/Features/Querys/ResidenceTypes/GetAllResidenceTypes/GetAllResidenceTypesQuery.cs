6e80597b02d466a13e22634e268b99f108219783
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 14:52:06 -0600

    Add query to get all residence types

---
+++ VehicleBrands.Application/Features/Querys/ResidenceTypes/GetAllResidenceTypes/GetAllResidenceTypesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.ResidenceTypes.GetAllResidenceTypes
+{
+    public class GetAllResidenceTypesQuery : IRequest<List<GenericResponseVM>>
+    {
+    }
+}
