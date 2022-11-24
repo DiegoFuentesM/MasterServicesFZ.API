cc939852f48b55b097ece2446e1233f4c7edeb44
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 14:17:40 -0600

    Add repository residence type

---
+++ VehicleBrands.Infrastructure/Repositories/ResidenceTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class ResidenceTypeRepository : SqlRepositoryBase<ResidenceType>, IResidenceTypeRepository
+    {
+        public ResidenceTypeRepository(SqlDbContext context) : base(context)
+        {
+        }
+    }
+}
