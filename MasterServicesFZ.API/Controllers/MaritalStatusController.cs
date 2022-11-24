c0c8468cc8e6a09574b96750e8375e4af2eae0a4
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 14:13:58 -0600

    Add marital status controller

---
+++ MasterServicesFZ.API/Controllers/MaritalStatusController.cs
@@ -0,0 +1,30 @@
+﻿using MasterServicesFZ.Application.Features.Querys.MaritalStatuses.GetAllMaritalStatusesQuery;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class MaritalStatusController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MaritalStatusController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(GenericResponseVM), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetAll()
+        {
+            var maritalStatuses = await _mediator.Send(new GetAllMaritalStatusesQuery());
+            return Ok(maritalStatuses);
+        }
+    }
+}
