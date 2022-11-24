180089b1a9723b3fea53ccad99f9399235aa1290
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 15:49:52 -0600

    Add repository international business

---
+++ VehicleBrands.Infrastructure/Repositories/InternationalBusinessRepository.cs
@@ -0,0 +1,13 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class InternationalBusinessRepository : RepositoryBase<InternationalBusiness>, IInternationalBusinessRepository
+    {
+        public InternationalBusinessRepository(PostgresDbContext context) : base(context)
+        {
+        }
+    }
+}
