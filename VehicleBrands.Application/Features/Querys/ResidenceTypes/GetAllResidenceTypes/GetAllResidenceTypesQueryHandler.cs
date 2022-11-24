0951fd9634808c524a7bd620c4f6b73e9ff52dc9
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 14:52:18 -0600

    Add query handler to get all residence types

---
+++ VehicleBrands.Application/Features/Querys/ResidenceTypes/GetAllResidenceTypes/GetAllResidenceTypesQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterServicesFZ.Application.Features.Querys.ResidenceTypes.GetAllResidenceTypes
+{
+    public class GetAllResidenceTypesQueryHandler : IRequestHandler<GetAllResidenceTypesQuery, List<GenericResponseVM>>
+    {
+        private readonly IResidenceTypeRepository _residenceTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllResidenceTypesQueryHandler(IResidenceTypeRepository residenceTypeRepository, IMapper mapper)
+        {
+            _residenceTypeRepository = residenceTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GenericResponseVM>> Handle(GetAllResidenceTypesQuery request, CancellationToken cancellationToken)
+        {
+            var listResidenceTypes = await _residenceTypeRepository.GetAllAsync();
+            var residenceTypes = _mapper.Map<List<GenericResponseVM>>(listResidenceTypes).OrderBy(x => x.Value).ToList();
+            return residenceTypes;
+        }
+    }
+}
