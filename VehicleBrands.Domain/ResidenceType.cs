cb67a5731e158b47ad44db2dd79707a09393c85a
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-24 14:13:25 -0600

    Add residence type model

---
+++ VehicleBrands.Domain/ResidenceType.cs
@@ -0,0 +1,9 @@
+﻿namespace MasterServicesFZ.Domain
+{
+    public class ResidenceType
+    {
+        public int CodigoLP { get; set; }
+        public string Descripcion { get; set; }
+        public int CodigoScr { get; set; }
+    }
+}
