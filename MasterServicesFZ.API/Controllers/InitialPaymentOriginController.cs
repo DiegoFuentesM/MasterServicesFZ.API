dadc428208dbe3fb1927820142c1068e507640c4
diegoFm10 <diego479@gmail.com>
2022-11-25 11:26:09 -0500

    Add profession and initial payment origin controllers

---
+++ MasterServicesFZ.API/Controllers/InitialPaymentOriginController.cs
@@ -0,0 +1,26 @@
+﻿using MasterServicesFZ.Application.Features.Querys.InitialPaymentOrigin.GetAllInitialPaymentOrigin;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class InitialPaymentOriginController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public InitialPaymentOriginController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllInitialPaymentOrigins()
+        {
+            var query = await _mediator.Send(new GetAllInitialPaymentOriginQuery());
+            return Ok(query);
+        }
+    }
+}
---
+++ MasterServicesFZ.API/Controllers/ProfessionController.cs
@@ -0,0 +1,26 @@
+﻿using MasterServicesFZ.Application.Features.Querys.Professions.GetAllProfessions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProfessionController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ProfessionController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProfessions()
+        {
+            var query = await _mediator.Send(new GetAllProfessionsQuery());
+
+            return Ok(query);
+        }
+    }
+}
