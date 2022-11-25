44ec2addeb3eccd5bc8fa6f7267fc268f7921412
diegoFm10 <diego479@gmail.com>
2022-11-25 11:27:19 -0500

    Add profession and initial payment origin interface repositories

---
+++ VehicleBrands.Application/Contracts/Persistence/IProfessionRepository.cs
@@ -0,0 +1,8 @@
+﻿using MasterServicesFZ.Domain;
+
+namespace MasterServicesFZ.Application.Contracts.Persistence
+{
+    public interface IProfessionRepository : IAsyncRepository<Profession>
+    {
+    }
+}
