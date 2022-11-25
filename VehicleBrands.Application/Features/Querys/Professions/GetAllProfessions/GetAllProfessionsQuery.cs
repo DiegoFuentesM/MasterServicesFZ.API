f8c21033ba74538be957f04039a693248189a540
diegoFm10 <diego479@gmail.com>
2022-11-25 11:28:02 -0500

    Add professions query and handler

---
+++ VehicleBrands.Application/Features/Querys/Professions/GetAllProfessions/GetAllProfessionsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.Professions.GetAllProfessions
+{
+    public class GetAllProfessionsQuery : IRequest<List<GenericResponseVM>>
+    {
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/Professions/GetAllProfessions/GetAllProfessionsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.Professions.GetAllProfessions
+{
+    public class GetAllProfessionsQueryHandler : IRequestHandler<GetAllProfessionsQuery, List<GenericResponseVM>>
+    {
+        private readonly IProfessionRepository _professionRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllProfessionsQueryHandler(IProfessionRepository professionRepository, IMapper mapper)
+        {
+            _professionRepository = professionRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GenericResponseVM>> Handle(GetAllProfessionsQuery request, CancellationToken cancellationToken)
+        {
+            var professions = await _professionRepository.GetAllAsync();
+            return _mapper.Map<List<GenericResponseVM>>(professions).OrderBy(x => x.Value).ToList();
+        }
+    }
+}
