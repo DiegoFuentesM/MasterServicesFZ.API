e37a1ab19b9c86aef19b083c9fa4ca557d5b11ff
diegoFm10 <diego479@gmail.com>
2022-11-25 11:28:25 -0500

    Add profession and initial payment origin models

---
+++ VehicleBrands.Domain/Profession.cs
@@ -0,0 +1,10 @@
+﻿namespace MasterServicesFZ.Domain
+{
+    public class Profession
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public bool? activo { get; set; }
+        public bool? visible { get; set; }
+    }
+}
