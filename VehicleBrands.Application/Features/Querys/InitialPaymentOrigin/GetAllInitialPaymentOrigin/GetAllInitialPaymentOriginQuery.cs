13afc623ebffe8ff57fea13a7b8af7f6dfd8f98e
diegoFm10 <diego479@gmail.com>
2022-11-25 11:27:44 -0500

    Add initial payment origin query and handler

---
+++ VehicleBrands.Application/Features/Querys/InitialPaymentOrigin/GetAllInitialPaymentOrigin/GetAllInitialPaymentOriginQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.InitialPaymentOrigin.GetAllInitialPaymentOrigin
+{
+    public class GetAllInitialPaymentOriginQuery : IRequest<List<GenericResponseVM>>
+    {
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/InitialPaymentOrigin/GetAllInitialPaymentOrigin/GetAllInitialPaymentOriginQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.InitialPaymentOrigin.GetAllInitialPaymentOrigin
+{
+    public class GetAllInitialPaymentOriginQueryHandler : IRequestHandler<GetAllInitialPaymentOriginQuery, List<GenericResponseVM>>
+    {
+        private readonly IInitialPaymentOriginRepository _initialPaymentOriginRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllInitialPaymentOriginQueryHandler(IInitialPaymentOriginRepository initialPaymentOriginRepository, IMapper mapper)
+        {
+            _initialPaymentOriginRepository = initialPaymentOriginRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GenericResponseVM>> Handle(GetAllInitialPaymentOriginQuery request, CancellationToken cancellationToken)
+        {
+            var initialPaymentOrigins = await _initialPaymentOriginRepository.GetAllAsync();
+            return _mapper.Map<List<GenericResponseVM>>(initialPaymentOrigins).OrderBy(o => o.Value).ToList();
+        }
+    }
+}
