de1d1bf15502f5e514f1c9b857259ecde928cfc0
diegoFm10 <diego479@gmail.com>
2022-11-25 11:31:37 -0500

    Add profession and initial payment origin repositories

---
+++ VehicleBrands.Infrastructure/Repositories/ProfessionRepository.cs
@@ -0,0 +1,20 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    public class ProfessionRepository : RepositoryBase<Profession>, IProfessionRepository
+    {
+        public ProfessionRepository(PostgresDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Profession>> GetAllProfessionsQuery ()
+        {
+            var professions = await _context.m_profesion!.Where(p => p.activo == true && p.visible == true).ToListAsync();
+            return professions;
+        }
+    }
+}
