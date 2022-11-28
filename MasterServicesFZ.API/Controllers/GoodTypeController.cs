fd95dfec925798895814d99bb458967ff856e2fd
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-28 11:44:46 -0600

    Add good type controller

---
+++ MasterServicesFZ.API/Controllers/GoodTypeController.cs
@@ -0,0 +1,31 @@
+﻿using MasterServicesFZ.Application.Features.Querys.GoodTypes.GetAllGoodTypes;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MasterServicesFZ.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class GoodTypeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public GoodTypeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(GenericResponseVM), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GeAll()
+        {
+            var goodTypes = await _mediator.Send(new GetAllGoodTypesQuery());
+            return Ok(goodTypes);
+        }
+    }
+}
