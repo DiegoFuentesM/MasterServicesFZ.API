cb7a5bae93195e695ffa4a9ef35f37b6e5b007f4
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-28 11:46:38 -0600

    Add repository good types

---
+++ VehicleBrands.Infrastructure/Repositories/GoodTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Domain;
+using MasterServicesFZ.Infrastructure.Persistence;
+
+namespace MasterServicesFZ.Infrastructure.Repositories
+{
+    internal class GoodTypeRepository : RepositoryBase<GoodType>, IGoodTypeRepository
+    {
+        public GoodTypeRepository(PostgresDbContext context) : base(context)
+        {
+        }
+    }
+}
