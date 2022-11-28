bf89d709a4b07384176f4983b2212a9292f48b79
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-28 11:49:22 -0600

    Add good type query and handler

---
+++ VehicleBrands.Application/Features/Querys/GoodTypes/GetAllGoodTypes/GetAllGoodTypesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.GoodTypes.GetAllGoodTypes
+{
+    public class GetAllGoodTypesQuery : IRequest<List<GenericResponseVM>>
+    {
+    }
+}
---
+++ VehicleBrands.Application/Features/Querys/GoodTypes/GetAllGoodTypes/GetAllGoodTypesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MasterServicesFZ.Application.Contracts.Persistence;
+using MasterServicesFZ.Application.Features.Shared.VMs;
+using MediatR;
+
+namespace MasterServicesFZ.Application.Features.Querys.GoodTypes.GetAllGoodTypes
+{
+    public class GetAllGoodTypesQueryHandler : IRequestHandler<GetAllGoodTypesQuery, List<GenericResponseVM>>
+    {
+        private readonly IGoodTypeRepository _goodTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllGoodTypesQueryHandler(IGoodTypeRepository goodTypeRepository, IMapper mapper)
+        {
+            _goodTypeRepository = goodTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GenericResponseVM>> Handle(GetAllGoodTypesQuery request, CancellationToken cancellationToken)
+        {
+            var listGoodTypes = await _goodTypeRepository.GetAllAsync();
+            var goodTypes = _mapper.Map<List<GenericResponseVM>>(listGoodTypes).OrderBy(g => g.Value).ToList();
+            return goodTypes;
+        }
+    }
+}
