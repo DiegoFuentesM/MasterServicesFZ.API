de96bfacdf085306bafb325c1fc227a8df2555e5
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-29 19:21:22 -0600

    Add service registration

---
+++ MasterServicesFZ.Auth/ServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using MasterServicesFZ.Application.Contracts.Auth;
+using MasterServicesFZ.Auth.Handlers;
+using MasterServicesFZ.Auth.Models;
+using MasterServicesFZ.Auth.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MasterServicesFZ.Auth
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddAuthServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IClientService, ClientService>();
+            services.AddScoped<ICacheService, CacheService>();
+
+            services.Configure<ApikeyClientsSettings>(configuration.GetSection("ApikeyClientsSettings"));
+
+            services.AddScoped<ApiKeyAuthenticationHandler>();
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = ApiKeyAuthenticationOptions.DefaultScheme;
+            }).AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>(ApiKeyAuthenticationOptions.DefaultScheme, o => { });
+
+            services.AddMemoryCache();
+            return services;
+        }
+    }
+}
