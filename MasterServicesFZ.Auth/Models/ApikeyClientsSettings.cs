8f197666156549c30b7dee3728c413c3a8c37d1a
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-29 19:19:35 -0600

    Add api key models

---
+++ MasterServicesFZ.Auth/Models/ApikeyClientsSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MasterServicesFZ.Auth.Models
+{
+    public class ApikeyClientsSettings
+    {
+        public string UrlBase { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
